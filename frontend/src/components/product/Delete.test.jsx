1d7ad650964e90b82061a66ef0d411a17eab882b
dzyaka2002 <108496922+dzyaka2002@users.noreply.github.com>
2025-06-05 22:06:58 +0300

    Update Delete.test.jsx

--- frontend/src/components/product/Delete.test.jsx
+++ frontend/src/components/product/Delete.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter, Route } from 'react-router-dom';
-import ProductDelete from './ProductDelete';
+import ProductDelete from './Delete';
 import * as Service from './Service';
 
 // Мокаем модуль Service
